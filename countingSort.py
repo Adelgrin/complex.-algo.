753853dccfc62d04833fea1cbbb0bced68207e97
Adelgrin <50162397+Adelgrin@users.noreply.github.com>
2025-03-26 01:24:26 +0000

    foda

---
+++ countingSort.py
@@ -0,0 +1,26 @@
+def countingSort(V):
+    count = []
+    for i in range(maxVal(V)+1):
+        count.append(0)
+    acu = [-1]
+    for j in V:
+        count[j] += 1
+    for k in range(1,len(count)):
+        acu.append(acu[k-1]+count[k])
+    saida = []
+    for l in range(len(V)):
+        saida.append(0)
+    for m in range(len(V)-1,-1,-1):
+        saida[m] = acu.index(m)
+        acu[saida[m]] -= 1
+    return saida
+
+def maxVal(V):
+    max = V[0]
+    for i in V:
+        if i > max:
+            max = i
+    return max
+teste = [7,9,7,5,3,2,8,5,4,12,10,10,6]
+print(countingSort(teste))
+#bgl é loco
