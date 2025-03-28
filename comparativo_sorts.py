19a83eb275fbcf65250c40cc79cd53fa5f6972ad
Adelgrin <50162397+Adelgrin@users.noreply.github.com>
2025-03-28 02:20:11 +0000

    comparativos

--- comparativo_sorts.py
+++ comparativo_sorts.py
@@ -3,24 +3,29 @@ from mergeSort import mergeSort
 from quickSort import quickSort
 from heapSort import heapSort
 from countingSort import countingSort
+from radixSort import radixSort
 
 vet = []
 for i in range(999,0,-1):
     vet.append(i)
 
 inicio = time.process_time()
-mergeSort(vet,0,len(vet)-1)
+mergeSort(vet[:],0,len(vet)-1)
 fim = time.process_time()
 print("merge ",fim-inicio)
 inicio = time.process_time()
-quickSort(vet,0,len(vet)-1)
+quickSort(vet[:],0,len(vet)-1)
 fim = time.process_time()
 print("quick ",fim-inicio)
 inicio = time.process_time()
-heapSort(vet)
+heapSort(vet[:])
 fim = time.process_time()
 print("heap ",fim-inicio)
 inicio = time.process_time()
-countingSort(vet)
+countingSort(vet[:])
 fim = time.process_time()
 print("count ",fim-inicio)
+inicio = time.process_time()
+radixSort(vet[:])
+fim = time.process_time()
+print("radix ",fim-inicio)
